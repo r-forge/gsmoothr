0c1e8a21d50b315a8555c84e65d3bffcadf19b95
markrobinsonca (Dr ) <mark.robinson.ca@gmail.com>
2009-05-20 22:49:05 +0000

    remove excess files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gsmoothr@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/tmeantest.h
+++
@@ -1,5 +0,0 @@
-#include <R.h>
-#include <Rinternals.h>
-
-//void tmean(double *x, double *xs, long *sp, long *n_, double *tr, long *_np, long *_pw);
-void tmean(double *x, double *xs, int *sp, int *n_, double *tr, int *_np, int *_pw);
