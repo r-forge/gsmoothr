8bf08b90258fddbae7f7fa96c7437d97adeed15b
markrobinsonca (Dr ) <mark.robinson.ca@gmail.com>
2009-05-22 06:10:27 +0000

    changed 'tmeanC' so that it can take NAs/NaNs and not bomb
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gsmoothr@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gsmoothr/R/tmeanC.R
+++ pkg/gsmoothr/R/tmeanC.R
@@ -2,10 +2,17 @@ tmeanC<-function(sp, x, nProbes=10, probeWindow=600, trim=0.1) {
   # void tmean(double *x, double *xs, long *sp, long *n_, double *tr, long *_np, long *_pw)
 
   n <- length(x)
+  outx <- rep(NA,n)
+
   stopifnot( length(x)==length(sp) )
 
-  out<-.C("tmean", x=as.double(x), xs=double(n), sp=as.integer(sp), n=as.integer(n), 
+  # screen out NA and NaNs before sending to C
+  k <- !is.na(x) & !is.nan(x)
+  n <- sum(k)
+
+  out<-.C("tmean", x=as.double(x[k]), xs=double(n), sp=as.integer(sp[k]), n=as.integer(n), 
                    tr=as.double(trim), np=as.integer(nProbes), pw=as.integer(probeWindow), PACKAGE="gsmoothr")
 
-  out$xs
+  outx[k] <- out$xs
+  outx
 }
