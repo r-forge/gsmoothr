30c243e1874381036c5f5ff9aedcf61233705b44
aarsta (Aaron Statham) <a.statham@garvan.org.au>
2009-05-24 08:38:42 +0000

    loadSampleDirectory can now handle Cy3 & Cy5 processNDF can now handle both types of reverse strand NDFs that I have seen
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gsmoothr@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/epitools/R/processNimblegenArrays.R
+++ pkg/epitools/R/processNimblegenArrays.R
@@ -1,14 +1,16 @@
 processNDF <- function(filename) {
 	ndfTemp <- read.table(filename, sep="\t", header=T, stringsAsFactors=F)
+	#determine if reverse strand probe are marked by _RS or RS	
+	if (length(grep("_RS", ndfTemp$PROBE_ID))>0) rsSymbol = "_RS" else rsSymbol="RS"
 
-	chr <- gsub("FS.*|_RS.*","",ndfTemp$PROBE_ID)
+	chr <- gsub(paste("FS.*|",rsSymbol,".*",sep=""),"",ndfTemp$PROBE_ID)
 	chr <- gsub("CHR", "chr", chr)
 	chr <- gsub("chr0", "chr", chr)
 
 	#return the centre of the probes sequence
 	position <- ndfTemp$POSITION+trunc(nchar(ndfTemp$PROBE_SEQUENCE)/2)
 	strand <- rep("+", nrow(ndfTemp))
-	strand[grep("_RS",ndfTemp$PROBE_ID)] = "-"
+	strand[grep(rsSymbol,ndfTemp$PROBE_ID)] = "-"
 
 	#return the GC content of each probe
 	GC <- sapply(ndfTemp$PROBE_SEQUENCE, function(x) {sum(strsplit(x,split="")[[1]] %in% c("C","G"))/nchar(x)}, USE.NAMES=F)
@@ -23,12 +25,21 @@ loadPairFile <- function(filename, ndf) {
 	return(log2(pairTemp$PM[match(ndf$index,pairTemp$Y*768+pairTemp$X)]))
 }
 
-loadSampleDirectory <- function(path, ndf) {
+loadSampleDirectory <- function(path, ndf, what="Cy3") {
+	#what="Cy3" or "Cy5" or "Cy3/Cy5" or "Cy5/Cy3"
+	if (!what %in% c("Cy3","Cy5","Cy3/Cy5","Cy5/Cy3")) stop('parameter "what" must be "Cy3", "Cy5", "Cy3/Cy5" or "Cy5/Cy3"')	
 	files <- dir(path, pattern=".pair$")
 	samples <- unique(gsub("_532.pair|_635.pair","",files))
 	tempData <- matrix(NA, nrow=nrow(ndf), ncol=length(samples), dimnames=list(NULL, samples))
 	for (i in 1:length(samples)) {
-		tempData[,i] <- loadPairFile(paste(path,"/",samples[i],"_532.pair",sep=""), ndf)
+		if (what=="Cy3")
+		  tempData[,i] <- loadPairFile(paste(path,"/",samples[i],"_532.pair",sep=""), ndf)
+		else if (what=="Cy5")
+		  tempData[,i] <- loadPairFile(paste(path,"/",samples[i],"_635.pair",sep=""), ndf)
+		else if (what=="Cy3/Cy5")
+		  tempData[,i] <- loadPairFile(paste(path,"/",samples[i],"_532.pair",sep=""), ndf)-loadPairFile(paste(path,"/",samples[i],"_635.pair",sep=""), ndf)
+		else if (what=="Cy3/Cy5")
+		  tempData[,i] <- loadPairFile(paste(path,"/",samples[i],"_635.pair",sep=""), ndf)-loadPairFile(paste(path,"/",samples[i],"_532.pair",sep=""), ndf)
 	}
 	return(tempData)
 }
