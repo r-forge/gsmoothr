2c848a672833984f854f5792db555e4e93d70392
dario (Dario Strbenac) <dariostrbenac@hotmail.com>
2009-09-01 23:22:53 +0000

    removed mistaken update od gPPDf.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gsmoothr@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/epitools/R/getProbePositionsDf.R
+++ pkg/epitools/R/getProbePositionsDf.R
@@ -12,24 +12,3 @@ getProbePositionsDf <- function(cdf, ..., verbose=-20) {
 
     data.frame(chr=chrs[as.character(ch)],position=sp,index=ind,stringsAsFactors=FALSE)
 }
-
-ndfFilePath <- dir(designDir, "090410_HG18_JS_ChIP.ndf", full = TRUE)
-ndfAll <- processNDF(ndfFilePath)
-ndfAll <- ndfAll[grepl("^chr|^SPIKE|^RANDOM", ndfAll$chr), ]
-columnsOnChip <- max(RGTobyAzaCopy$genes$X)
-matchIndices <- match(ndfAll$index, RGTobyAzaCopy$genes$Y * columnsOnChip + RGTobyAzaCopy$genes$X)
-RGTobyAzaCopy <- RGTobyAzaCopy[matchIndices, ]
-colnames(RGTobyAzaCopy$G) <- RGTobyAzaCopy$targets$Cy3
-rownames(RGTobyAzaCopy$G) <- RGTobyAzaCopy$genes$PROBE_ID
-RGTobyAzaCopy$G <- normalizeQuantiles(RGTobyAzaCopy$G)
-
-
-data.frame(chr=ch,position=sp,stringsAsFactors=FALSE)
-
-arrayColourSpotNames <- RG$targets$Cy3
-designMatrix <- matrix(0, nrow = length(arrayColourSpotNames), ncol = 1,
-dimnames = list(arrayColourSpotNames, "LNCAP Aza - PREC Aza"))
-LNCapIP <- grep("LNCaP", arrayColourSpotNames)
-designMatrix[LNCapIP, 1] <- 1/length(LNCapIP)
-PrECIP <- grep("PrEC", arrayColourSpotNames)
-designMatrix[PrECIP, 1] <- -1/length(PrECIP)
