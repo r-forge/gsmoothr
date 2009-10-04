09a5afcbaa3533876767c633ec4b3a7471501772
markrobinsonca (Dr ) <mark.robinson.ca@gmail.com>
2009-10-04 23:39:09 +0000

    couple changes to cpgDensityCalc()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gsmoothr@61 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/epitools/R/cpgDensityCalc.R
+++ pkg/epitools/R/cpgDensityCalc.R
@@ -3,7 +3,7 @@ cpgDensityCalc <- function(locationsTable, windowSize)
 	require(MEDME)
 	require(BSgenome.Hsapiens.UCSC.hg18)
 
-	dummy <- matrix(nrow=dim(locationsTable)[1], ncol=1)
+	dummy <- matrix(nrow=dim(locationsTable)[1], ncol=2)
 	rownames(dummy) <- 1:nrow(dummy)
 
 	mms <- new("MEDMEset",chr=as.character(locationsTable$chr),pos=locationsTable$position,logR=dummy,organism="hsa")
