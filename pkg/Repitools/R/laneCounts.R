ca27830a8648ffac8cec0b528a8b262eb32ccf20
markrobinsonca (Dr ) <mark.robinson.ca@gmail.com>
2010-01-20 05:11:41 +0000

    fixed the problem I introduced into laneCounts()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gsmoothr@109 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 51261189bc250efcad427c4c7a83a52fc4b665d0

--- pkg/Repitools/R/laneCounts.R
+++ pkg/Repitools/R/laneCounts.R
@@ -8,10 +8,10 @@ laneCounts <- function(cs) {
 	stopifnot("GenomeDataList" %in% class(cs))
 	#require(IRanges)
 	#require(BSgenome)
-	#lib.sizes <- IRanges::as.list(BSgenome::gdapply(cs, function(x) length(unlist(x))))
-	#lib.sizes <- lapply(lib.sizes, IRanges::as.list)
-	#lib.sizes <- sapply(lib.sizes, function(x) sum(unlist(x)))
-	#return(lib.sizes)	
         # modification by Mark R -- Jan 20 2010
-        sapply(cs, FUN=function(u) sum( sapply(u, FUN=function(v) length(v[["+"]])+length(v[["-"]])) ) )
+	lib.sizes <- IRanges::as.list(BSgenome::gdapply(cs, function(x) length(x[["+"]]) + length(x[["-"]]) ))
+	lib.sizes <- lapply(lib.sizes, IRanges::as.list)
+	sapply(lib.sizes, function(x) sum(unlist(x)))
+        #chrSizes <- gdapply(cs, FUN=function(v) length(v[["+"]])+length(v[["-"]]))
+        #gdapply(cs, FUN=function(u) sum( sapply(u, FUN=function(v) length(v[["+"]])+length(v[["-"]])) ) )
 }
