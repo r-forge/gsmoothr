6be7408c8d3e5612329c672098a735611852daf6
markrobinsonca (Dr ) <mark.robinson.ca@gmail.com>
2010-01-20 04:13:59 +0000

    little change to quicken laneCount()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gsmoothr@108 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: d30fa57f1036a99c4f291b13bdaa2d3bc76ebfc3

--- pkg/Repitools/R/laneCounts.R
+++ pkg/Repitools/R/laneCounts.R
@@ -6,10 +6,12 @@
 
 laneCounts <- function(cs) {
 	stopifnot("GenomeDataList" %in% class(cs))
-	require(IRanges)
-	require(BSgenome)
-	lib.sizes <- IRanges::as.list(BSgenome::gdapply(cs, function(x) length(unlist(x))))
-	lib.sizes <- lapply(lib.sizes, IRanges::as.list)
-	lib.sizes <- sapply(lib.sizes, function(x) sum(unlist(x)))
-	return(lib.sizes)	
+	#require(IRanges)
+	#require(BSgenome)
+	#lib.sizes <- IRanges::as.list(BSgenome::gdapply(cs, function(x) length(unlist(x))))
+	#lib.sizes <- lapply(lib.sizes, IRanges::as.list)
+	#lib.sizes <- sapply(lib.sizes, function(x) sum(unlist(x)))
+	#return(lib.sizes)	
+        # modification by Mark R -- Jan 20 2010
+        sapply(cs, FUN=function(u) sum( sapply(u, FUN=function(v) length(v[["+"]])+length(v[["-"]])) ) )
 }
