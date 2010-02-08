70211df2b14908a1fd2baf1413df72201457296c
dario (Dario Strbenac) <dariostrbenac@hotmail.com>
2010-02-08 08:28:41 +0000

    K9Ac Precs reinstated. Initialisation script removed.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gsmoothr@121 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: e52e20443cdb5d8cc5035f04e89874ec86e5a597

--- pkg/RepitoolsExamples/R/setupExamples.R
+++ pkg/RepitoolsExamples/R/setupExamples.R
@@ -1,15 +1,15 @@
 setupExamples <- function(doCopy=FALSE) {
 	dataDirectory <- system.file("exampleData", package="RepitoolsExamples")
-	if  (file.access(dataDirectory, 2)) {
+	if  (file.access(dataDirectory, 2) == 0) {
 		setwd(dataDirectory)
 		cat("Successfully setwd to example data directory\n")
 		invisible(NULL)
 	} else {
 		cat("Example data directory is not writable\n")
 		if (doCopy) {
-			cat("Copying example data directory to current working directory\n")
+			cat("Copying example data directory to current working directory\n")	
 			require(R.utils)
-			copyDirectory(dataDirectory)
+			file.copy(dataDirectory, getwd(), recursive = TRUE)
 			cat("Successfully setwd to local copy of example data directory\n")
 			invisible(NULL)
 		} else cat("If you wish to make a local copy, run setupExamples(doCopy=TRUE)\n")
