9bb409d9d37470dbe4bcefddef93810ef80f8d2f
dario (Dario Strbenac) <dariostrbenac@hotmail.com>
2010-02-23 02:34:04 +0000

    mappabilityCalc function and documentation file new to the package.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gsmoothr@125 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: da8219f7165922fc12bf5e93efbb11c24756df69

---
+++ pkg/Repitools/R/mappabilityCalc.R
@@ -0,0 +1,23 @@
+mappabilityCalc <- function(locationsTable, windowSize = 500)
+{
+	require(BSgenome.Hsapiens.UCSC.hg18)
+	require(BSgenome.Hsapiens36bp.UCSC.hg18mappability)
+	
+	offFarEdge <- logical()
+	for (rowIndex in 1:nrow(locationsTable))
+		offFarEdge[rowIndex] <- locationsTable$position[rowIndex] > (length(Hsapiens[[as.character(locationsTable$chr[rowIndex])]]) - windowSize)
+	if(locationsTable$position < windowSize || any(offFarEdge))
+		stop("Not all locations' windows are obtainable. Remove locations that are too close to the edge of chromosomes.")
+
+	locationsByChr <- split(locationsTable$position, locationsTable$chr)
+	
+	mapScores <- list()
+	for(chromosomeIndex in 1:length(locationsByChr))
+	{
+		currentlocations <- locationsByChr[[chromosomeIndex]]
+		currentSeq <- Hsapiens36bp[[names(locationsByChr)[chromosomeIndex]]]
+		mapScores[[chromosomeIndex]] <- sapply(currentlocations, function(currentPosition){indices <- (currentPosition - windowSize) : (currentPosition + windowSize); 1-alphabetFrequency(currentSeq[indices], as.prob=TRUE)[["N"]]})
+	}
+
+	return(unsplit(mapScores, locationsTable$chr))
+}
