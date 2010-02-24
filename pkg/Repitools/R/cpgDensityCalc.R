ca4be542cd7524a5ba11522194487f6b1e8ae027
dario (Dario Strbenac) <dariostrbenac@hotmail.com>
2010-02-24 00:47:25 +0000

    Aarons changes now merged into cpg and mappability calcs. i.e. BSgenome object is passed in.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gsmoothr@128 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 23dd4b7f81c7af49f653fa67be9dc2545ae55b4b

--- pkg/Repitools/R/cpgDensityCalc.R
+++ pkg/Repitools/R/cpgDensityCalc.R
@@ -1,27 +1,5 @@
-cpgDensityCalc <- function(locationsTable, windowSize=500, wFunction=c("linear","exp","log","none"), organism = "Hsapiens")
+cpgDensityCalc <- function(locationsTable, windowSize=500, wFunction=c("linear","exp","log","none"), organism)
 {
-
-	pkgName <- switch(organism,
-		Hsapiens = "BSgenome.Hsapiens.UCSC.hg18",
-		Amellifera = "BSgenome.Amellifera.UCSC.apiMel2",
-		Athaliana = "BSgenome.Athaliana.TAIR.04232008",
-		Btaurus = "BSgenome.Btaurus.UCSC.bosTau4",
-		Celegans = "BSgenome.Celegans.UCSC.ce2",
-		Cfamiliaris = "BSgenome.Cfamiliaris.UCSC.canFam2",
-		Dmelanogaster = "BSgenome.Dmelanogaster.UCSC.dm3",
-		Drerio = "BSgenome.Drerio.UCSC.danRer5",
-		Ecoli = "BSgenome.Ecoli.NCBI.20080805",
-		Ggallus = "BSgenome.Ggallus.UCSC.galGal3",
-		Mmusculus = "BSgenome.Mmusculus.UCSC.mm9",
-		Ptroglodytes = "BSgenome.Ptroglodytes.UCSC.panTro2",
-		Rnorvegicus = "BSgenome.Rnorvegicus.UCSC.rn4",
-		Scerevisiae = "BSgenome.Scerevisiae.UCSC.sacCer2",
-	)
-	if(is.null(pkgName))
-		stop("Error : The organism name you provided does not match any of the known sequences. See ?cpgDensityCalc for supported organism names.")
-
-	do.call(require, list(pkgName))
-
 	if(any(locationsTable$position < (windowSize / 2)))
 		stop("Not all locations' windows are obtainable. Remove locations that are too close to the edge of the start of chromosomes.")
 
@@ -29,14 +7,14 @@ cpgDensityCalc <- function(locationsTable, windowSize=500, wFunction=c("linear",
 
 	for(chrIndex in 1:length(locationsByChr))
 	{
-		endOfChromosome <- length(Hsapiens[[as.character(names(locationsByChr[chrIndex]))]])
+		endOfChromosome <- length(organism[[as.character(names(locationsByChr[chrIndex]))]])
 		offFarEdge <- sapply(locationsByChr[[chrIndex]], function(location) location > endOfChromosome - windowSize / 2)
 		if(any(offFarEdge))
 			stop("Not all locations' windows are obtainable. Remove locations that are too close to the end of chromosomes.")
 	}
 	
 	cpgDensity <- numeric()
-	sequences <- do.call(getSeq, list(x = get(organism), names = locationsTable$chr, start = locationsTable$position - windowSize / 2, end = locationsTable$position + windowSize / 2))
+	sequences <- do.call(getSeq, list(x = organism, names = locationsTable$chr, start = locationsTable$position - windowSize / 2, end = locationsTable$position + windowSize / 2))
 	CGfinds <- gregexpr("CG", sequences, fixed = TRUE)
 	if(wFunction == "none") {
 		cpgDensity <- sapply(CGfinds, length)
--- pkg/Repitools/R/mappabilityCalc.R
+++ pkg/Repitools/R/mappabilityCalc.R
@@ -1,8 +1,5 @@
-mappabilityCalc <- function(locationsTable, windowSize = 500)
-{
-	require(BSgenome.Hsapiens.UCSC.hg18)
-	require(BSgenome.Hsapiens36bp.UCSC.hg18mappability)
-	
+mappabilityCalc <- function(locationsTable, windowSize = 500, organism)
+{	
 	if(any(locationsTable$position < (windowSize / 2)))
 		stop("Not all locations' windows are obtainable. Remove locations that are too close to the edge of the start of chromosomes.")
 
@@ -10,7 +7,7 @@ mappabilityCalc <- function(locationsTable, windowSize = 500)
 
 	for(chrIndex in 1:length(locationsByChr))
 	{
-		endOfChromosome <- length(Hsapiens[[as.character(names(locationsByChr[chrIndex]))]])
+		endOfChromosome <- length(organism[[as.character(names(locationsByChr[chrIndex]))]])
 		offFarEdge <- sapply(locationsByChr[[chrIndex]], function(location) location > endOfChromosome - windowSize / 2)
 		if(any(offFarEdge))
 			stop("Not all locations' windows are obtainable. Remove locations that are too close to the end of chromosomes.")
@@ -19,9 +16,9 @@ mappabilityCalc <- function(locationsTable, windowSize = 500)
 	mapScores <- list()
 	for(chromosomeIndex in 1:length(locationsByChr))
 	{
-		currentlocations <- locationsByChr[[chromosomeIndex]]
-		currentSeq <- Hsapiens36bp[[names(locationsByChr)[chromosomeIndex]]]
-		mapScores[[chromosomeIndex]] <- sapply(currentlocations, function(currentPosition){indices <- (currentPosition - windowSize) : (currentPosition + windowSize); 1-alphabetFrequency(currentSeq[indices], as.prob=TRUE)[["N"]]})
+		currentLocations <- locationsByChr[[chromosomeIndex]]
+		currentSeq <- organism[[names(locationsByChr)[chromosomeIndex]]]
+		mapScores[[chromosomeIndex]] <- sapply(currentLocations, function(currentPosition){indices <- (currentPosition - windowSize) : (currentPosition + windowSize); 1-alphabetFrequency(currentSeq[indices], as.prob=TRUE)[["N"]]})
 	}
 
 	return(unsplit(mapScores, locationsTable$chr))
