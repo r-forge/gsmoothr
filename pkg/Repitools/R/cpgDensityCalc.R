a11657d246e490e11bfbfc2003f62a7bf097bd1b
aarsta (Aaron Statham) <a.statham@garvan.org.au>
2010-02-25 05:43:11 +0000

    * Made cpgDensityCalc an S3 method, works on data.frames, GenomeData & GenomeDataLists - but is *very* slow for seq data (~1 min per million reads!) * Made cpgDensityPlot - works on GenomeDataList * Bumped version number 0.9.0
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gsmoothr@133 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: d2b23a69789110e0dc4d8a9b1b243ecc6d4de73c

--- pkg/Repitools/R/cpgDensityCalc.R
+++ pkg/Repitools/R/cpgDensityCalc.R
@@ -1,40 +1,52 @@
-cpgDensityCalc <- function(locationsTable, windowSize=500, wFunction=c("linear","exp","log","none"), organism)
-{
+setMethodS3("cpgDensityCalc", "GenomeDataList", function(rs, seqLen, ...) {
+	return(lapply(IRanges::as.list(rs), cpgDensityCalc, seqLen, ...))
+})
+
+
+setMethodS3("cpgDensityCalc", "GenomeData", function(rs, seqLen, ...) {
+	rs.midpt <- vector(mode='list', length=length(rs))
+	names(rs.midpt) <- names(rs)
+	
+	for (chr in names(rs)) {
+		rs.midpt[[chr]] <- data.frame(chr=chr, position=c(rs[[chr]][["+"]]+seqLen/2, rs[[chr]][["-"]]-seqLen/2), stringsAsFactors=FALSE)
+	}
+	rs.midpt <- do.call(rbind, rs.midpt)
+	
+	return(cpgDensityCalc(rs.midpt, window=seqLen, ...))
+			
+})
+
+setMethodS3("cpgDensityCalc", "data.frame", function(locations, window=500, wFunction=c("linear","exp","log","none"), organism, shift=TRUE, ...) {
 	gregexpr.2 <- function(...) sapply(gregexpr(...), function(x) {
 					if (x[1]=="-1") return(integer(0))
 					else return(x)
 				}, USE.NAMES = FALSE)
 
 	wFunction <- match.arg(wFunction)
-	if(any(locationsTable$position < (windowSize / 2)))
-		stop("Not all locations' windows are obtainable. Remove locations that are too close to the edge of the start of chromosomes.")
 
-	locationsByChr <- split(locationsTable$position, locationsTable$chr)
-
-	for(chrIndex in 1:length(locationsByChr))
-	{
-		endOfChromosome <- length(organism[[as.character(names(locationsByChr[chrIndex]))]])
-		offFarEdge <- sapply(locationsByChr[[chrIndex]], function(location) location > endOfChromosome - windowSize / 2)
-		if(any(offFarEdge))
-			stop("Not all locations' windows are obtainable. Remove locations that are too close to the end of chromosomes.")
+	chr.length <- seqlengths(organism)
+	if(any((locations$position<window/2) | (locations$position+window/2>chr.length[locations$chr]))) {
+		if (!shift) stop("Not all locations' windows are within chromosome boundaries")
+		warning("Not all locations' windows are within chromosome boundaries, shifting.")
+		locations$position[locations$position<window/2] <- window/2
+		locations$position <- ifelse((locations$position+window/2)>chr.length[locations$chr], chr.length[locations$chr]-window/2, locations$position)
 	}
-	
-	cpgDensity <- numeric()
-	sequences <- getSeq(x = organism, names = locationsTable$chr, start = locationsTable$position - (windowSize / 2) + 1, end = locationsTable$position + windowSize / 2)
+
+	sequences <- getSeq(x = organism, names = locations$chr, start = locations$position - (window / 2) + 1, end = locations$position + window / 2)
 	CGfinds <- gregexpr.2("CG", sequences, fixed = TRUE)
 	if(wFunction == "none") {
 		cpgDensity <- sapply(CGfinds, length)	
 	} else {
-		distances <- lapply(CGfinds, function(positionsInRegion) {abs(positionsInRegion - windowSize / 2)})
+		distances <- lapply(CGfinds, function(positionsInRegion) {abs(positionsInRegion - window / 2)})
 		if(wFunction == "linear") {
-			cpgDensity <- sapply(distances, function(distancesInRegion) sum(1 - (distancesInRegion / (windowSize / 2))))
+			cpgDensity <- sapply(distances, function(distancesInRegion) sum(1 - (distancesInRegion / (window / 2))))
 		} else if(wFunction == "log") {
-			cpgDensity <- sapply(distances, function(distancesInRegion) sum(log2(2 - (distancesInRegion / (windowSize / 2)))))
+			cpgDensity <- sapply(distances, function(distancesInRegion) sum(log2(2 - (distancesInRegion / (window / 2)))))
 		} else { # Exponential decay was requested.
-			cpgDensity <- sapply(distances, function(distancesInRegion) sum(exp(-5 * distancesInRegion / (windowSize / 2))))	
+			cpgDensity <- sapply(distances, function(distancesInRegion) sum(exp(-5 * distancesInRegion / (window / 2))))	
 		}
 	}
 	rm(sequences, CGfinds)
 	gc()
 	return(cpgDensity)
-}
+})
