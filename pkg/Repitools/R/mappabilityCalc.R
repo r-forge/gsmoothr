b9a92ae462960e23a9be74ca120469dfeb03a1b9
markrobinsonca (Dr ) <mark.robinson.ca@gmail.com>
2010-03-12 01:08:11 +0000

    delete Repitools
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gsmoothr@157 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: c0fff5d474b2741863ee7158d3a5d7f781e68ef3

--- pkg/Repitools/R/mappabilityCalc.R
+++
@@ -1,3 +0,0 @@
-mappabilityCalc <- function(locations, window = 500, organism, verbose=FALSE, chunkSize=10000000) {	
-	return(sequenceCalc(locations, window, organism, DNAString("N"), verbose=verbose, chunkSize=chunkSize)/window)
-}
